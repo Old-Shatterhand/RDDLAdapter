8805d69fe36102edce2705ba1295153fdb5d34ee
Roman Joeres <roman@luke.cs.uni-saarland.de>
2020-09-15 13:46:50 +0200

    Build-Errors fixed now arise SIGSEV

--- prost/src/search/adapter.cpp
+++ prost/src/search/adapter.cpp
@@ -1,34 +1,35 @@
-#include "states.h"
+#include "adapter.h"
+
+#include "../states.h"
 
 
-DLRSAdapter::DLRSAdapter(_name) : ProbabilisticSearchEngine(_name){
+DLRSAdapter::DLRSAdapter(std::string _name) : ProbabilisticSearchEngine(_name){
 	input.open(in_pipe);
 	pstream.open(out_pipe);
 }
 
 void DLRSAdapter::initSession(){
-	assert(input.good() && pstream.good());
-
-	std::cout << "printing tasks" << std::endl;
-    out << numberOfActions;
-    std::cout << "printed: number of actions: " << num_actions << std::endl;
+    assert(input.good() && pstream.good());
+    std::cout << "printing tasks" << std::endl;
+    pstream << numberOfActions;
+    std::cout << "printed: number of actions: " << numberOfActions << std::endl;
 
     for(int i = 0 ; i < numberOfActions ; ++i){
-        out << i;
+        pstream << i;
         std::cout << "printed: " << i << "-th action: " << i << std::endl;
     }
 
     std::cout << "printing vars" << std::endl;
-    out << State::numberOfDeterministicStateFluents + State::numberOfProbabilisticStateFluents;
-    std::cout << "printed: number of state_variables: " << state_variables->size() << std::endl;
+    pstream << State::numberOfDeterministicStateFluents + State::numberOfProbabilisticStateFluents;
+    std::cout << "printed: number of state_variables: " << State::numberOfDeterministicStateFluents + State::numberOfProbabilisticStateFluents << std::endl;
 
     for(int i = 0 ; i < State::numberOfDeterministicStateFluents + State::numberOfProbabilisticStateFluents ; ++i){
-        out << i;
+        pstream << i;
         std::cout << "printed: " << i << "-th state variable has 2 domains" << std::endl;
 
         for(int j = 0 ; j < 2 ; ++j){
-            out << j;
-            std::cout << "printed: " << i << "-th state, " << j << "-th value: " << *sit << std::endl;
+            pstream << j;
+            std::cout << "printed: " << i << "-th state, " << j << "-th value: " << j << std::endl;
         }
     }
     /*
@@ -49,36 +50,35 @@ void DLRSAdapter::initStep(State const& current){
 	printState(current);
 }
 
-void DLRSAdapter::finishStep(int immediateReward){
+void DLRSAdapter::finishStep(double/* immediateReward*/){
 	// remember that the next method called from the IPCClient->Planner->SearchEngine is the finishRound
 }
 
-void DLRSAdapter::finishStep(vector<double> *successor, double immediateReward){
+void DLRSAdapter::finishStep(std::vector<double> *successor, const double& immediateReward){
     pstream << immediateReward;
     printState(successor);
 }
 
 void DLRSAdapter::finishRound(double roundReward){
-	out << roundReward;
+	pstream << roundReward;
 	std::cout << "Round reward" << roundReward;
-	out << "";
+	pstream << "";
 }
 
-void DLRSAdapter::finishSession(double totalReward){
-    input.flush();
+void DLRSAdapter::finishSession(double/* totalReward*/){
     input.close();
 
     pstream.flush();
     pstream.close();
 }
 
-void DLRSAdapter::estimateBestActions(State const& _rootState, std::vector<int>& bestActions){
+void DLRSAdapter::estimateBestActions(State const&/* _rootState*/, std::vector<int>& bestActions){
     // print executable actions
-    out << numberOfActions;
-    std::cout << "printed: number of actions: " << num_actions << std::endl;
+    pstream << numberOfActions;
+    std::cout << "printed: number of actions: " << numberOfActions << std::endl;
 
     for(int i = 0 ; i < numberOfActions ; ++i){
-        out << i;
+        pstream << i;
         std::cout << "printed: " << i << "-th action: " << i << std::endl;
     }
 
@@ -87,26 +87,26 @@ void DLRSAdapter::estimateBestActions(State const& _rootState, std::vector<int>&
 	bestActions.emplace_back(std::atoi(action));
 }
 
-void DLRSAdapter::printState(State const& state){
-    std::vector<double> stateVariables();
+void DLRSAdapter::printState(State const&/* state*/){
+    std::vector<double> stateVariables;
 
     for (unsigned int i = 0; i < State::numberOfDeterministicStateFluents; ++i) {
-        deterministicStateFluents.push_back(state.getStateFluent(i));
+        //stateVariables.emplace_back(state.getStateFluent(i));
     }
-    for (unsigned int i = 0; i < numberOfProbabilisticStateFluents; ++i) {
-        probabilisticStateFluents.push_back(state.getStateFluent(i + numberOfDeterministicStateFluents));
+    for (unsigned int i = 0; i < State::numberOfProbabilisticStateFluents; ++i) {
+        //stateVariables.emplace_back(state.getStateFluent(i + State::numberOfDeterministicStateFluents));
     }
 
     printState(&stateVariables);
 }
 
-void DLRSAdapter::printState(vector<double> *stateVariables){
-    std::cout << "printing state"
-    out << stateVariables->size();
+void DLRSAdapter::printState(std::vector<double> *stateVariables){
+    std::cout << "printing state";
+    pstream << stateVariables->size();
     std::cout << "printed: state size: " << stateVariables->size() << std::endl;
 
     for(auto it = stateVariables->begin() ; it != stateVariables->end() ; ++it){
-        out << *it;
+        pstream << *it;
         std::cout << "printed state variable: " << *it << std::endl;
     }
 }
